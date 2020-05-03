a53b7d2dcfdad2c67428576b6e0302836a50d815
itayargi <itayhaza@gmail.com>
2020-05-03 22:53:27 +0300

    Updates

---
+++ precache-manifest.4e25db85107e21d1ba1b9dbe7853a6be.js
@@ -0,0 +1,150 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50d8fb1f82f0d5b31aead2154037229c",
+    "url": "/willing2/index.html"
+  },
+  {
+    "revision": "39dbcb17d95324e27a05",
+    "url": "/willing2/static/css/2.787f2ed5.chunk.css"
+  },
+  {
+    "revision": "74922a1c185e41d71d20",
+    "url": "/willing2/static/css/main.daf6e600.chunk.css"
+  },
+  {
+    "revision": "39dbcb17d95324e27a05",
+    "url": "/willing2/static/js/2.4ef89ac3.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/willing2/static/js/2.4ef89ac3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "74922a1c185e41d71d20",
+    "url": "/willing2/static/js/main.c894ec52.chunk.js"
+  },
+  {
+    "revision": "6c56759f0e31cb0c7a93",
+    "url": "/willing2/static/js/runtime-main.6d98c3b4.js"
+  },
+  {
+    "revision": "b3d7e8adb99c30e73bb695ae7374a45b",
+    "url": "/willing2/static/media/Case.b3d7e8ad.svg"
+  },
+  {
+    "revision": "dc117eee08b0d32617db45698316dd62",
+    "url": "/willing2/static/media/Emergency.dc117eee.svg"
+  },
+  {
+    "revision": "e870928e1f2fa522ba29980886c1eefd",
+    "url": "/willing2/static/media/General.e870928e.svg"
+  },
+  {
+    "revision": "5ec7e2c67db85c7a7509061a1c7e9f6f",
+    "url": "/willing2/static/media/Give away.5ec7e2c6.svg"
+  },
+  {
+    "revision": "324b6948886f9fafe06eee3d2d56cf29",
+    "url": "/willing2/static/media/Item needed.324b6948.svg"
+  },
+  {
+    "revision": "868c3507c7c4f43d858d41264a66ce20",
+    "url": "/willing2/static/media/Lost and found.868c3507.svg"
+  },
+  {
+    "revision": "2f00ad42f2891a0701674331264d76f5",
+    "url": "/willing2/static/media/Onboarding_01.2f00ad42.png"
+  },
+  {
+    "revision": "2f0ef873487cac1d84991cfdfdcb6489",
+    "url": "/willing2/static/media/Ride_Delivery.2f0ef873.svg"
+  },
+  {
+    "revision": "06957b96b4f936b5a0a2cf0597a355af",
+    "url": "/willing2/static/media/Road assist.06957b96.svg"
+  },
+  {
+    "revision": "e78aa76a5f66ca4b530a8d560b1d02fa",
+    "url": "/willing2/static/media/Social.e78aa76a.svg"
+  },
+  {
+    "revision": "0f130b05b248a62070621a52aabecacf",
+    "url": "/willing2/static/media/background.0f130b05.svg"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/willing2/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/willing2/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/willing2/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/willing2/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/willing2/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "e5880c6529296925f7fb7e546fd2d63a",
+    "url": "/willing2/static/media/countdown.e5880c65.svg"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/willing2/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/willing2/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/willing2/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/willing2/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/willing2/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/willing2/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/willing2/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/willing2/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/willing2/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/willing2/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/willing2/static/media/outline-icons.ef60a4f6.woff"
+  },
+  {
+    "revision": "e7c12cbea6b7b12246df4dd222dd4134",
+    "url": "/willing2/static/media/verified.e7c12cbe.svg"
+  },
+  {
+    "revision": "1fcdc4eec564011b719ff88438256717",
+    "url": "/willing2/static/media/willing.1fcdc4ee.png"
+  }
+]);
